fb1a8fd0e6ad3f2d1271e787f519e2da2f9bcf93
yjw1339515 <47889178+yjw1339515@users.noreply.github.com>
2019-03-01 17:31:07 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,3 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-//后台默认显示页面
-Route::get('admin','admin\IndexController@Index');
-
-//后台用户添加
-Route::get('admin/users/create','admin\UsersController@create');
-
-Route::post('admin/users/store','admin\UsersController@store');
-//后台默认显示页面
-Route::get('/admin/users/index','admin\UsersController@index');
-//后台修改数据
-Route::get('/admin/users/edit/{id}','admin\UsersController@edit');
-Route::PUT('/admin/users/update/{id}','admin\UsersController@update');
