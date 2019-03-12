58e563c20539a19af4d7dabb647fab5ce706d7c7
Liuweihan <415758762@qq.com>
2019-03-12 19:04:04 +0800

    刘伟翰第六次提交-修改前台页面

--- resources/views/home/phb/index.blade.php
+++ resources/views/home/phb/index.blade.php
@@ -1,16 +1,19 @@
 @extends('home.layout')
 @section('content')
-<div class="top_block1">
+<div class="top_block1" style="background-image:url('/static/home/images/bg.png');">
     <div class="top_title">
-        <h2 class="top_title_h2" style="color:#DC143C;text-align:center;font-size:45px;font-family:微软雅黑;margin-top:65px;">白库排行榜TOP 10</h2>
-        <h3 class="top_title_h3" style="color:black;text-align:center;font-size:18px;font-family:微软雅黑;margin-top:40px;">全站商品，一网打尽！更加超值，更多优惠！折上加折，惊喜不断！</h3>
-        <h3 class="top_title_h3" style="color:black;text-align:center;font-size:18px;font-family:微软雅黑;margin-top:40px;">为您推荐今日最佳好物！</h3>
+        <h2 class="top_title_h2" style="color:#796a5f;text-align:center;font-size:38px;font-family:微软雅黑;padding-top:65px;">白库排行榜TOP 10</h2>
+        <h3 class="top_title_h3" style="color:#DC143C;text-align:center;font-size:16px;font-family:微软雅黑;margin-top:40px;">全站商品，一网打尽！更加超值，更多优惠！折上加折，惊喜不断！</h3>
+        <h3 class="top_title_h3" style="color:#DC143C;text-align:center;font-size:16px;font-family:微软雅黑;margin-top:40px;">为您推荐今日最佳好物！</h3>
     </div>
     <div class="top_body">
     @foreach($data as $k=>$v)
     @if(($v->tid)!=0)
     <div class="top_goods" style="padding-top:60px;">
         <a href="/home/goods/detail/{{$v->id}}">
+            <div class="goods_top" style="background-color:#796a5f;width:98%;height:35px;color:#D3D3D3;font-size:24px;line-height:32px;text-indent:50px;margin-bottom:15px;">
+                今日推荐——好物第{{$v->tid}}名
+            </div>
             <div class="goods_smpic" style="height:309px;width:309px;float:left;">
                 <img src="/static/home/images/{{$v->gpic}}" alt="{{$v->gname}}" style="height:259px;width:259px;margin-left:25px;margin-bottom:25px;margin-left:25px;margin-right:25px;"/>
             </div>
